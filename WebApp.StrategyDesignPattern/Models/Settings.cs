73eb145ae553f8e5972bd24e99d780bd91d87220
tugbaakan <akan.tugba@gmail.com>
2021-07-03 14:30:22 +0300

    strategy design pattern

---
+++ WebApp.StrategyDesignPattern/Models/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.StrategyDesignPattern.Models
+{
+    public class Settings
+    {
+        public static string claimDatabaseType = "databasetype";
+        public EDatabaseType DatabaseType;
+        public EDatabaseType GetDefaultDatabaseType = EDatabaseType.SQLServer;
+    }
+}
---
+++ WebApp.StrategyDesignPattern/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.StrategyDesignPattern.Models;
+
+namespace WebApp.StrategyDesignPattern.Repositories
+{
+    public interface IProductRepository
+    {
+
+        Task<Product> GetById(string id);
+        Task<List<Product>> GetAllByUserId(string userId);
+        Task<Product> Save(Product product);
+        Task Update(Product product);
+        Task Delete(Product product);
+
+    }
+}
